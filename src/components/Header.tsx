e8634cb1616ea2c2cd531c1dc6fcc440c13576b8
Thiago Padovani <padovanitr@gmail.com>
2022-03-11 23:26:53 -0300

    feat: reorganize components structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,25 @@
 import { Image, Flex } from '@chakra-ui/react'
+import { useMediaQuery } from "@chakra-ui/react"
 
 export function Header(){
+    const [isMobile] = useMediaQuery("(max-width: 768px)")
+
     return(
         <Flex
             as="header"
             w="100%"
-            h="100"
+            h={isMobile ? '50px' : "100px"}
             mx="auto"
             px="6"
             align="center"
             alignItems="center"
             justifyContent="center"
         >
-            <Image src="Logo.svg" alt="Logo" />
+            <Image 
+                boxSize={isMobile ? '81px' : '180px'} 
+                src="Logo.svg" 
+                alt="Logo" 
+            />
         </Flex>
     )
 }
